acccf5b14af2d43c83236a2478212c8a2fe65478
Jongha Weon <amanjk110528@gmail.com>
2023-09-26 11:01:40 +0900

    [Gold I] Title: 구간 합 구하기, Time: 1360 ms, Memory: 145096 KB -BaekjoonHub

---
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -0,0 +1,52 @@
+import sys
+
+
+def st_init(t, nums, start, end, idx):
+    if start == end:
+        t[idx] = nums[start]
+        return t[idx]
+    else:
+        mid = (start + end) // 2
+        t[idx] = st_init(t, nums, start, mid, idx * 2) + st_init(t, nums, mid + 1, end, idx * 2 + 1)
+        return t[idx]
+
+
+def st_update(t, pos, data, start, end, idx):
+    if start == end:
+        tmp, t[idx] = data - t[idx], data
+        loopy = idx // 2
+        while loopy > 0:
+            t[loopy] += tmp
+            loopy //= 2
+        return
+    mid = (start + end) // 2
+    if pos <= mid:
+        st_update(t, pos, data, start, mid, idx * 2)
+    else:
+        st_update(t, pos, data, mid + 1, end, idx * 2 + 1)
+
+
+def st_sum(t, rge_s, rge_e, start, end, idx):
+    if rge_s > end or rge_e < start:
+        return 0
+    if rge_s == start and rge_e == end:
+        return t[idx]
+    mid = (start + end) // 2
+    if rge_e <= mid:
+        return st_sum(t, rge_s, rge_e, start, mid, idx * 2)
+    elif rge_s > mid:
+        return st_sum(t, rge_s, rge_e, mid + 1, end, idx * 2 + 1)
+    else:
+        return st_sum(t, rge_s, mid, start, mid, idx * 2) + st_sum(t, mid + 1, rge_e, mid + 1, end, idx * 2 + 1)
+
+
+N, M, K = tuple(map(int, sys.stdin.readline().rstrip().split(' ')))
+L = [int(sys.stdin.readline().rstrip()) for _ in range(N)]
+tree = [0 for _ in range(N * 4)]
+st_init(tree, L, 0, N - 1, 1)
+for _ in range(M + K):
+    a, b, c = tuple(map(int, sys.stdin.readline().rstrip().split(' ')))
+    if a == 1:
+        st_update(tree, b, c, 1, N, 1)
+    elif a == 2:
+        print(st_sum(tree, b, c, 1, N, 1))
