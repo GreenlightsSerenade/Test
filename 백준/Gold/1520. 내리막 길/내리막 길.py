6f330b546fe52c5fae60edaa0c70de6e34d91eb7
Jongha Weon <amanjk110528@gmail.com>
2025-04-17 10:30:54 +0900

    [Gold III] Title: 내리막 길, Time: 148 ms, Memory: 49332 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,20 +1,28 @@
 import sys
+from collections import deque, defaultdict
+from itertools import combinations
+import heapq
+sys.setrecursionlimit(10 ** 8)
 
+def solution(n, m, g):
+    dxdy = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+    DP = [[-1 for _ in range(m)] for _ in range(n)]
 
-def solution(n, m, map):
-    ans = [[-1 for _ in range(m)] for _ in range(n)]
-    def DFS(x, y):
-        if x == n - 1 and y == m - 1:
+    def DFS(nx, ny):
+        if nx == n - 1 and ny == m - 1:
             return 1
-        if ans[x][y] != -1:
-            return ans[x][y]
-        ans[x][y] = 0
-        d = [(0, -1), (0, 1), (1, 0), (-1, 0)]
-        for dx, dy in d:
-            xx, yy = x + dx, y + dy
-            if 0 <= xx < n and 0 <= yy < m and map[x][y] > map[xx][yy]:
-                ans[x][y] += DFS(xx, yy)
-        return ans[x][y]
+        if DP[nx][ny] != -1:
+            return DP[nx][ny]
+
+        ret = 0
+        for dx, dy in dxdy:
+            x, y = nx + dx, ny + dy
+            if 0 <= x < n and 0 <= y < m and g[nx][ny] > g[x][y]:
+                ret += DFS(x, y)
+
+        DP[nx][ny] = ret
+        return DP[nx][ny]
+
     return DFS(0, 0)
 
 
